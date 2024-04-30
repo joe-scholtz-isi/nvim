f8133c870185eb17f7a783fa3d926c9ef39e4098
Joe Ferreira Scholtz <joe.scholtz@senairs.org.br>
2024-04-30 08:54:39 -0300

    commit before dotfiles

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -143,43 +143,61 @@ return {
     end,
 
     -- C/C++
-    opts = function()
-      local dap = require("dap")
-      if not dap.adapters["codelldb"] then
-        require("dap").adapters["codelldb"] = {
-          type = "server",
-          host = "localhost",
-          port = "${port}",
-          executable = {
-            command = "codelldb",
-            args = {
-              "--port",
-              "${port}",
-            },
+    --   opts = function()
+    --     local dap = require("dap")
+    --     if not dap.adapters["codelldb"] then
+    --       require("dap").adapters["codelldb"] = {
+    --         type = "server",
+    --         host = "localhost",
+    --         port = "${port}",
+    --         executable = {
+    --           command = "codelldb",
+    --           args = {
+    --             "--port",
+    --             "${port}",
+    --           },
+    --         },
+    --       }
+    --     end
+    --     for _, lang in ipairs({ "c", "cpp" }) do
+    --       dap.configurations[lang] = {
+    --         {
+    --           type = "codelldb",
+    --           request = "launch",
+    --           name = "Launch file",
+    --           program = function()
+    --             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --           end,
+    --           cwd = "${workspaceFolder}",
+    --         },
+    --         {
+    --           type = "codelldb",
+    --           request = "attach",
+    --           name = "Attach to process",
+    --           processId = require("dap.utils").pick_process,
+    --           cwd = "${workspaceFolder}",
+    --         },
+    --       }
+    --     end
+    --   end,
+    {
+      "linux-cultist/venv-selector.nvim",
+      cmd = "VenvSelect",
+      opts = function(_, opts)
+        if LazyVim.has("nvim-dap-python") then
+          opts.dap_enabled = true
+        end
+        return vim.tbl_deep_extend("force", opts, {
+          name = {
+            "venv",
+            ".venv",
+            "env",
+            ".env",
           },
-        }
-      end
-      for _, lang in ipairs({ "c", "cpp" }) do
-        dap.configurations[lang] = {
-          {
-            type = "codelldb",
-            request = "launch",
-            name = "Launch file",
-            program = function()
-              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "codelldb",
-            request = "attach",
-            name = "Attach to process",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-        }
-      end
-    end,
+        })
+      end,
+      keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    }
   },
   {
     "williamboman/mason.nvim",
